229720bbe7c07a6006253887e67bb88c9108d8a7
Kabindra <s.kabindra@gmail.com>
2021-12-10 14:16:01 -0600

    2nd week assignment

---
+++ 2ndWeekAssignment.py
@@ -0,0 +1,22 @@
+##Display the text “Hello World! I wonder why that is always the
+# default coding text to start with”
+print('Hello World! I wonder why that is always the  default coding text to start with')
+# Add two numbers together
+print(1+2)
+# Subtract a number from another number
+print(5-2)
+# Multiply two numbers
+print(2*3)
+# Divide between two numbers
+print(10/2)
+# Concatenate two strings together (any words)
+print("Hello"+" world")
+# Create a list of 4 items (can be strings, numbers, both)
+v = [1,2,3,4]
+
+# Append an item to your list (again, can be a string, number)
+v.append(5)
+print(v)
+##Create a tuple with 4 items (can be strings, numbers, both)
+t1 = (1, 2, 3, 4)
+print(type(t1))
